6a5c94066947b8426d93e0fd12b6afa96b12966f
Kayra <KayraAlat@gmail.com>
2015-04-13 19:05:13 +0100

    Initial commit

---
+++ wp-content/themes/naked-wordpress-master/footer.php
@@ -0,0 +1,29 @@
+<?php
+	/*-----------------------------------------------------------------------------------*/
+	/* This template will be called by all other template files to finish 
+	/* rendering the page and display the footer area/content
+	/*-----------------------------------------------------------------------------------*/
+?>
+
+</div><!-- / end page container, begun in the header -->
+
+<footer class="site-footer" role="contentinfo">
+	<div class="site-info container">
+		
+		<p><a href="http://kayra.co.uk" rel="theme">Kayra Alat 2015</a>
+		</p>
+
+		<!-- Birthed on the Naked Wordpress theme by Joshua Beckman -->
+		<!-- Code, copy, photos and videos by Kayra Alat -->
+		
+	</div><!-- .site-info -->
+</footer><!-- #colophon .site-footer -->
+
+<?php wp_footer(); 
+// This fxn allows plugins to insert themselves/scripts/css/files (right here) into the footer of your website. 
+// Removing this fxn call will disable all kinds of plugins. 
+// Move it if you like, but keep it around.
+?>
+
+</body>
+</html>
