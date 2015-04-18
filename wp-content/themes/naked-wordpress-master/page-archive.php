23a6929fd8f7545f30820c941969a17a486b99b6
Kayra <KayraAlat@gmail.com>
2015-04-18 18:28:32 +0100

    Very basic archive listing functionality added

--- wp-content/themes/naked-wordpress-master/page-archive.php
+++ wp-content/themes/naked-wordpress-master/page-archive.php
@@ -1,6 +1,6 @@
 <?php 
 /**
- * 	Template Name: Home Page
+ * 	Template Name: Archive
  *
  *	This page template has a sidebar built into it, 
  * 	and can be used as a home page, in which case the title will not show up.
@@ -12,34 +12,24 @@ get_header(); // This fxn gets the header.php file and renders it ?>
 <div class="main-fluid"><!-- start the page containter -->
 	<div id="primary" class="row-fluid">
 		<div id="content" role="main" class="span8 offset2">
-			<?php if ( have_posts() ) : 
-			// Do we have any posts/pages in the databse that match our query?
-			?>
+			<?php if ( have_posts() ) : // Do we have any posts/pages in the databse that match our query? ?>
 
-				<?php while ( have_posts() ) : the_post(); 
-				// If we have a page to show, start a loop that will display it
-				?>
+				<?php while ( have_posts() ) : the_post(); // If we have a page to show, start a loop that will display it ?>
 
 					<article class="post">
 
-						<?php if (!is_front_page()) : // Only if this page is NOT being used as a home page, display the title ?>
-							<h1 class='title'>
-								<?php the_title(); // Display the page title ?>
-							</h1>
-						<?php endif; ?>
-										
-						<div class="the-content">
-							<?php the_content(); 
-							// This call the main content of the page, the stuff in the main text box while composing.
-							// This will wrap everything in paragraph tags
-							?>
-							
-							<?php wp_link_pages(); // This will display pagination links, if applicable to the page ?>
-						</div><!-- the-content -->
-						
+						<h1 class="title"><?php the_title(); ?></h1>
+
+						<?php get_search_form(); ?>
+
+						<ul>
+							<?php wp_get_archives('type=postbypost'); ?>
+						</ul> 	
+
+
 					</article>
 
-				<?php endwhile; // OK, let's stop the page loop once we've displayed it ?>
+				<?php endwhile; // OK, let's stop the post loop once we've displayed it ?>
 
 			<?php else : // Well, if there are no posts to display and loop through, let's apologize to the reader (also your 404 error) ?>
 				
@@ -47,7 +37,8 @@ get_header(); // This fxn gets the header.php file and renders it ?>
 					<h1 class="404">Nothing has been posted like that yet</h1>
 				</article>
 
-			<?php endif; // OK, I think that takes care of both scenarios (having a page or not having a page to show) ?>
+			<?php endif; // OK, I think that takes care of both scenarios (having a post or not having a post to show) ?>
+
 		</div><!-- #content .site-content -->
 	<!--	<div id="sidebar" role="sidebar" class="span4">
 			<?php get_sidebar(); // This will display whatever we have written in the sidebar.php file, according to admin widget settings ?>
