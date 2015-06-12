aafd935c431b7911654bec8c8f3089cdf88d63f5
Kayra <KayraAlat@gmail.com>
2015-06-12 01:31:37 +0100

    Successfully edited the comments form

--- wp-content/themes/apacheblack/single.php
+++ wp-content/themes/apacheblack/single.php
@@ -17,15 +17,36 @@
 		<button class="trigger"><span>Trigger</span></button>
 		<div class="title">
 			<h1><?php the_title(); ?></h1>
-			<h5 class="quote"><?php the_field('subtitle'); ?></h5>
-			<p>by <strong><?php the_author(); ?></strong> &#8212; Posted in <strong><?php the_category(', '); ?></strong> on <strong><?php the_time('F j, Y'); ?></strong></p>
+			<p class='subtitle'><?php the_field('subtitle'); ?></p>
+			<p>Posted in <strong><?php the_category(', '); ?></strong> on <strong><?php the_time('F j, Y'); ?></strong></p>
 		</div>
 
-		<article class="content" ?>">
-			<div class="col-md-6 col-md-offset-3">
-				<?php the_content(); ?>
+		<article class="content">
+			<div class="utility-margin-top col-md-6 col-md-offset-3">
+				<div class="content-text">
+					<?php the_content(); ?>
+				</div>
 				<hr/>
-				<?php comments_template(); ?>
+
+				<!-- Editing the comments form -->
+
+				<?php $comments_args = array(
+
+					'fields' => apply_filters('comment_form_default_fields', array(
+						'author' => '<input id="author" name="author" type="text" placeholder ="Name" value="' . 
+        							esc_attr( $commenter['comment_author'] ) . '" size="30"' . $aria_req . ' />',
+						'email' => '<input id="email" name="email" type="text" placeholder="Email (won\'t appear)" value="' . 
+									esc_attr(  $commenter['comment_author_email'] ) . '" size="30"' . $aria_req . ' />',
+						'url' => '',
+						)),
+					'comment_field' => '<textarea id="comment" name="comment" placeholder="Comment" cols="45" rows="8" 
+										aria-required="true"></textarea>',
+					'label_submit' => 'Publish comment',
+        			'comment_notes_after' => '',
+
+				) ?>
+
+				<?php comment_form($comments_args); ?>
 			</div>
 		</article>
 	</div>
