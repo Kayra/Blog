007edd33caaea4f9a6d6ddf15e2e3853bedf4790
Kayra <KayraAlat@gmail.com>
2015-06-29 22:08:50 +0100

    Removed ipad scrolling issue on post page fix

--- wp-content/themes/apacheblack/js/post.js
+++ wp-content/themes/apacheblack/js/post.js
@@ -139,7 +139,6 @@
 
         window.addEventListener( 'scroll', scrollPage );
         trigger.addEventListener( 'click', function() { toggle( 'reveal' ); } );
-        $(window).on(‘touchstart’, enable_scroll);
     });
 
 })(jQuery, this);
