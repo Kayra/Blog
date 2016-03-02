b5c03e93b048c27216c468fb06f4f74ca39a472f
Kayra <KayraAlat@gmail.com>
2016-03-02 01:57:17 +0000

    Formatted the archive

--- page-archive.php
+++ page-archive.php
@@ -13,13 +13,13 @@ Template Name: Archive
 <div class="col-md-6 col-md-offset-3">
 
 	<ul class="archive-list">
-		<?php 
+		<?php
 		$paged = (get_query_var('paged')) ? get_query_var('paged') : 1;
 		$args = array( 'post_type' => 'post', 'posts_per_page' => 10, 'paged' => $paged );
 		$wp_query = new WP_Query($args);
-		while ( have_posts() ) : the_post(); 
+		while ( have_posts() ) : the_post();
 		?>
-		    <li><h4><a href="<?php the_permalink() ?>" class="sliding-middle-out"><?php the_title(); ?> - <?php the_time('j F, Y'); ?></a></h4></li>
+		    <li><h4><a href="<?php the_permalink() ?>" class="sliding-middle-out"><?php the_title(); ?> - <?php the_time('F Y'); ?></a></h4></li>
 		<?php endwhile; wp_reset_query(); ?>
 
 		<!-- then the pagination links -->
